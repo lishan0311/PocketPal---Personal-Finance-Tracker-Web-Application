6cc2fac74f532a4272a28d9ee16d1d21e6084c62
lishan0311 <lishan0311@gmail.com>
2025-12-18 05:20:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const connectDB = require('./db')
 
 const app = express()
@@ -10,7 +9,7 @@ connectDB()
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 
 // router
 app.use('/api/auth', require('./routes/auth'))
